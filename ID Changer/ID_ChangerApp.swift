3216bf707d3d9b04ef09cc388dce97627650ef5e
leminlimez <jirizarry317@gmail.com>
2023-08-29 19:56:12 -0400

    Initial Commit

---
+++ ID Changer/ID_ChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ID_ChangerApp.swift
+//  ID Changer
+//
+//  Created by lemin on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ID_ChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
